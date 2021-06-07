42f8a98d724882b934275899e338dc2c116ca384
Rashedalam <rashedctg1719@gmail.com>
2021-06-07 21:37:26 +0600

    update

---
+++ src/main/java/dev/rashed/covidtrackerspringboot/CovidTrackerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.rashed.covidtrackerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTrackerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTrackerSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rashed/covidtrackerspringboot/CovidTrackerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rashed.covidtrackerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
