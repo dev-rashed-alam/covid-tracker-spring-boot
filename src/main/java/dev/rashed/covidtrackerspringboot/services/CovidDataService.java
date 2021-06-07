e7faed2b4ad9ec8fabaa9dbdd5aee4366f42ce41
Rashedalam <rashedctg1719@gmail.com>
2021-06-07 23:45:14 +0600

    woring with commonCSV lib

--- src/main/java/dev/rashed/covidtrackerspringboot/services/CovidDataService.java
+++ src/main/java/dev/rashed/covidtrackerspringboot/services/CovidDataService.java
@@ -1,6 +1,9 @@
 package dev.rashed.covidtrackerspringboot.services;
 
 import dev.rashed.covidtrackerspringboot.config.Config;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -15,17 +18,22 @@ import java.net.URL;
 public class CovidDataService {
 
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchData() throws IOException {
         StringBuilder responseContent = new StringBuilder();
+        BufferedReader bufferedReader;
         URL url = new URL(Config.API_URL);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
         if (connection.getResponseCode() == 200) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while (bufferedReader.readLine() != null) {
-                responseContent.append(bufferedReader.readLine());
+            bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(bufferedReader);
+            for (CSVRecord record : records) {
+                String state = record.get("Province_State");
+                System.out.println(state);
             }
             bufferedReader.close();
         }
-        System.out.println(responseContent.toString());
+        connection.disconnect();
     }
 }
